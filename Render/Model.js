97ea515c5ee9fc281ebfc748f29b6232db5420f2
FireDude123 <153016828+f1redood@users.noreply.github.com>
2026-03-06 09:23:10 -0500

    Update Model.js

--- Render/Model.js
+++ Render/Model.js
@@ -1,5 +1,16 @@
 export default class Model {
-  constructor(verts, inds) {
-    [this.verts, this.inds] = [verts, inds];
+  verts = [];
+  inds = [];
+  
+  constructor(verts, inds, uvs) {
+    for (var i = 0; i < verts.length; i++) {
+      this.verts.push(verts[i*3]);
+      this.verts.push(verts[i*3 + 1]);
+      this.verts.push(verts[i*3 + 2]);
+
+      this.verts.push(uvs[i*2]);
+      this.verts.push(uvs[i*2 + 1]);
+    }
+    this.inds = inds;
   }
 }
