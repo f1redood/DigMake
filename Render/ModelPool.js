5f50b0953e3af4a57612f914546051385dbb92cf
FireDude123 <153016828+f1redood@users.noreply.github.com>
2026-03-06 08:16:10 -0500

    Update ModelPool.js

--- Render/ModelPool.js
+++ Render/ModelPool.js
@@ -3,10 +3,11 @@ import Model from "https://f1redood.github.io/DigMake/Model.js";
 export default class ModelPool {
   verts = [];
   inds = [];
+  
   /**
-   * Adds a Model object to the ModelPool
+   * Adds a model to the ModelPool
    *
-   * @param {Model} The model to add to the ModelPool
+   * @param {Model|ModelPool} The model to add to the ModelPool
    */
   add(model) {
     this.verts.push(...model.verts);
